48af83c4cf1d13f64718e1288a7c0dca09f018d3
Alin Chindea <alin.chindea@gmail.com>
2018-04-23 15:33:39 +0100

    created a classical component, made a fetch call to the coins API

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/list/List';
 
 import './main.css';
 
 const App = () => {
-  const title = 'React Coin';
   return (
     <div>
       <Header />
-      
-      <h1>{title}</h1>
 
-      <h3>Up to date crypto currencies financial data</h3>
+      <List />
     </div>
   );
 };
