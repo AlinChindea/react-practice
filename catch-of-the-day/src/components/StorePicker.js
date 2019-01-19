db57eab86d629c5bf4aff2b3436814652749e18d
Alin Chindea <alin.chindea@gmail.com>
2019-01-19 21:49:23 +0000

    add logic to add sample fish to inventory using state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  
+  goToStore = event =>  {
+    // 1. stop the form from submitting on load
+    event.preventDefault();
+    // 2. get the text from the user's input
+    const storeName = this.myInput.current.value;
+    // 3. change the page to /store/whatever-input-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input 
+          type="text"
+          ref={this.myInput} 
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()} 
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
